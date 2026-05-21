88aea2dee43ccd7ee3b95861f431beab9b4f4ee3
Raquel Roriz <maria.raquel.roriz@gmail.com>
2026-05-21 23:50:13 +0200

    Add initial flow and first quiz question

--- quiz.py
+++ quiz.py
@@ -1,9 +1,16 @@
 print("Welcome to the Python Quiz!")
+answer_user = input("Would you like to take the quiz? (S/N): ")
 
-answer_user = input("Wold you like to take the quiz? (S/N): ")
-
-if answer_user.lower() != "s":
+if answer_user.lower() != 's':
     print("Maybe next time! Goodbye!")
     quit()
 
 print("Great! Let's get started!")
+print("What is Python? \n (A) Python is a versatile, high-level programming language known for its easy-to-read syntax and broad applications.\n (B) \n (C) \n (D) \n")
+
+answer_1 = input("Answer: ")
+
+if answer_1.upper() == 'A':
+    print("Correct!")
+else:
+    print("Incorrect")
